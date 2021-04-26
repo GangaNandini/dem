265c6ca087bffa6a027b715eec31e15fb48802c9
maheedhar-web <75677281+maheedhar-web@users.noreply.github.com>
2021-04-26 17:07:52 +0530

    First commit

---
+++ dem/src/com/abc/Launch.java
@@ -0,0 +1,9 @@
+package com.abc;
+
+public class Launch {
+ public static void main(String[] args)
+ {
+	 System.out.println(" hi welcome");
+ }
+
+}
---
+++ dem/src/com/abc/Launch1.java
@@ -0,0 +1,8 @@
+package com.abc;
+
+public class Launch1 {
+public static void main(String args[])
+{
+	System.out.println(" hellow");
+}
+}
